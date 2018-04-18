deb923e151714d486ac28997e0e31db25b91e387
Manoj Dobbala <manoj_k_dobbala@homedepot.com>
2018-04-17 22:11:41 -0500

    updated the landing page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,12 +21,12 @@ class Header extends Component {
         <div className={s.container}>
           <Navigation className={s.nav} />
           <Link className={s.brand} to="/">
-            <img src={require('./logo-small.png')} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
+            {/*<img src={require('./logo-small.png')} width="38" height="38" alt="React" />*/}
+            <span className={s.brandTxt}>Chore App</span>
           </Link>
           <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
+            <h1 className={s.bannerTitle}>Chores</h1>
+            <p className={s.bannerDesc}>From home repairs and moving to errands</p>
           </div>
         </div>
       </div>
